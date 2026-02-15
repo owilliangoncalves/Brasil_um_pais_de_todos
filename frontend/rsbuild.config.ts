bbd96329ebbe5d5378deecd54a321d307908edd0
Willian Goncalves <williangoncalves@Mac-mini-de-Willian.local>
2026-02-14 23:18:55 -0300

    arquivo criaod e preparado para usar TW 4^ + React 18^ + Loco no processo de build

---
+++ frontend/rsbuild.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  html: {
+    favicon: "src/assets/favicon.ico",
+    title: "Cidadão de Olho",
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5150",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
